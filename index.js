5ff1b92d6a62d353900849ae7bf4cdbbf28e1275
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-02-15 18:50:10 +0430

    add file

---
+++ index.js
@@ -0,0 +1,6 @@
+// const h1 = document.getElementsByTagName("h1");
+const h1 =document.querySelector("section>h1");
+const p = document.querySelector("div p");
+const div = document.querySelector("section + div");
+const id = document.getElementById("one");
+const divClass = document.getElementsByClassName("one");
