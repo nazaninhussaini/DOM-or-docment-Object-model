6606e2a9332ba32b2416c8cb8f87620573619e74
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-02-17 13:29:09 +0430

    chenges

---
+++ app.js
@@ -0,0 +1,8 @@
+const div = document.querySelector("div");
+const image = document.querySelector("img");
+const btn = document.querySelector("button");
+const UrlList = ["./image/p1.webp","./image/p2.jpg","./image/p3.jpeg","./image/p4.jpeg"];
+function chenger(){
+    
+}
+btn.addEventListener("click",chenger);
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // const h1 = document.getElementsByTagName("h1");
-const h1 =document.querySelector("section>h1");
+// const div = document.querySelector("section + div");
+const h1 =document.querySelectorAll("section h1");
+for (let i= 0; i<h1.length ; i++){
+    if(i%2 !== 0 ){
+        h1[i].classList.add("h1Style")
+    }
+    else{
+        h1[i].classList.add("meStyle")
+    }
+}
+
 const p = document.querySelector("div p");
-const div = document.querySelector("section + div");
-const id = document.getElementById("one");
+
 const divClass = document.getElementsByClassName("one");
+const ulList = document.querySelectorAll("section li");
+for (let li of ulList){
+    li.classList.add("li")
+}
+
+            
+             
