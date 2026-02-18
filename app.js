3c4d9c449d0f9a739286201bb5c8a0cf38e7c4f2
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-02-18 13:47:25 +0430

    chenges

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const div = document.querySelector("div");
 const image = document.querySelector("img");
 const btn = document.querySelector("button");
-const UrlList = ["./image/p1.webp","./image/p2.jpg","./image/p3.jpeg","./image/p4.jpeg"];
-function chenger(){
-    
-}
+const UrlList = ["./image/p1.webp","./image/p2.jpg","./image/p3.jpeg","./image/p4.jpeg"
+   ,"./image/p5.jpeg", "./image/p6.jpeg"
+];
 btn.addEventListener("click",chenger);
+let counter = 0;
+function chenger(){
+    if(counter< UrlList.length-1){
+        counter++
+    }
+    else{
+        counter = 0;
+    }
+    image.setAttribute("src",UrlList[counter])
+}
