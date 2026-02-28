332d4e1e47cc30211a9dd19fbb2ba503ff5d40b5
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-02-28 20:36:59 +0430

    chenges

---
+++ objectOriented programming/app.js
@@ -0,0 +1,9 @@
+class Mobaile {
+constructor(color,model,brand,RAM){
+    this.name = "mobaile";
+    this.color = color;
+    this.model = model;
+    this.brand = brand;
+    this.RAM = RAM;
+}
+}
