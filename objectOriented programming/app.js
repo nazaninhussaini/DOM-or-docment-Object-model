b1d0d1f401f66fd3c44db7e9a4160e1b24567e38
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-03-01 13:28:28 +0430

    chenges

--- objectOriented programming/app.js
+++ objectOriented programming/app.js
@@ -1,9 +1,12 @@
-class Mobaile {
+class Mobile {
 constructor(color,model,brand,RAM){
-    this.name = "mobaile";
+    this.name = "mobile";
     this.color = color;
     this.model = model;
     this.brand = brand;
     this.RAM = RAM;
 }
 }
+const mobile1 = new Mobile('black',"A12","Sumsung","5GB");
+const mobile2 = new Mobile('With',"14 Pro","Iphon","8GB");
+const mobile3 = new Mobile('Ston',"S22","S","16GB");
