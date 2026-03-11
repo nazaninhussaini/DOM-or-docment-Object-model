f524230b5e8f926f690495125387b1ecbdee32e2
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-03-11 05:58:26 +0430

    chenges codes

--- objectOriented programming/three.js
+++ objectOriented programming/three.js
@@ -1,3 +1,10 @@
+// import {list} from "./app.js"
+// import greeting from "./app.js"
+
+// list.map(person =>{
+//     console.log(person.name)
+// })
+// greeting()
 const person = {
     name: "Nazanin",
     lastName: "Hussaini",
@@ -5,3 +12,17 @@ const person = {
     score : 98
 }
 const{name , lastName , age,score} = person
+
+
+
+let Name = "Nazanin";
+Name.includes("z") && console.log("Hi theer");
+
+function User (name,lastName){
+    this.name = name;
+    this.lastName = lastName;
+}
+const user1 = new User("Mohammad","Hussaini");
+const user2 = new User("Mohammad Ali","Mohammadi");
+const user3 = new User("Mohammad Javad","Khaliqi");
+User.prototype.field = "Computer Science"
